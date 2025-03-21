bc03863c732dea03f156d5caeac0f50eb365baa0
Abhay <121509278+Abhay0090@users.noreply.github.com>
2025-03-22 02:37:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Allow all incoming and outgoing traffic
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -F
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -F
 
-# Start SSH service (Needs sudo since we're not root)
-sudo service ssh restart
+# Start SSH service
+service ssh restart
 
-# Authenticate Ngrok (Replace with your actual Ngrok token)
-ngrok authtoken 2lKjA15AAL3kFG0cbOpfTJGbewT_3PjMCSs55KCHQ2PKkoVdS
+# Authenticate Ngrok (Manually specify config path)
+export NGROK_CONFIG=/tmp/ngrok.yml
+ngrok authtoken 2lKjA15AAL3kFG0cbOpfTJGbewT_3PjMCSs55KCHQ2PKkoVdS --config $NGROK_CONFIG
 
-# Start Ngrok to expose **all ports**
+# Start Ngrok to expose all ports
 while true; do
-    ngrok tcp 0.0.0.0:1-65535 --log=stdout > /dev/null 2>&1
+    ngrok tcp 0.0.0.0:1-65535 --log=stdout --config $NGROK_CONFIG > /dev/null 2>&1
     sleep 2  # Restart delay in case of failure
 done &
 
